fc0026b77910a66408256e7bd7645f23b158eca5
ArpitaX <95682842+ArpitaX@users.noreply.github.com>
2021-12-08 11:51:35 +0530

    Update main.php

--- main.php
+++ main.php
@@ -48,10 +48,10 @@ $messagesec",
 	'reply_to_message_id'=> $message_id,
     'reply_markup'=>json_encode(['inline_keyboard' => [
         [
-          ['text' => "💠 Created By 💠", 'url' => "t.me/ninjanaveen"]
+          ['text' => "😏OWNER", 'url' => "t.me/ArpitaX"]
         ],
         [
-          ['text' => "💎 Source Code 💎", 'url' => "t.me/IndianBotsChat"]
+          ['text' => "💎 OUR CHANNEL 💎", 'url' => "t.me/Karding666"]
         ],
       ], 'resize_keyboard' => true])
         
@@ -124,7 +124,7 @@ if(strpos($message, "/cmds") === 0 || strpos($message, "!cmds") === 0){
 <b>/bin | !bin</b> - Bin Lookup
 <b>/iban | !iban</b> - IBAN Checker
   
-  <b>ϟ Join <a href='t.me/IndianBots'>IndianBots</a></b>",
+  <b>ϟ Join <a href='t.me/Karding666'>CHANNEL</a></b>",
     'parse_mode'=>'html',
     'disable_web_page_preview'=>true,
     'reply_markup'=>json_encode(['inline_keyboard'=>[
